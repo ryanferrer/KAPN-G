c1a1dc19d2f7572e05a3a23c55b0e78fc9c0e086
ryden <ryferr8122@gmail.com>
2019-04-20 16:58:57 -0500

    added dotenv and mongoose to package.json

--- server/server.js
+++ server/server.js
@@ -4,8 +4,12 @@ const port = 3000;
 
 app.use(express.static('dist'));
 
+// Serve HTML from dist upon get request to /
 app.get('/', (req, res) => {
   res.send('./dist/index.html');
 });
+
+// Request first 10 names from DB
+  // Require DB model
 
 app.listen(port, () => console.log(`KAPN-G listening on port ${port}`));
